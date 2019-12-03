982ba58798ef7d42a03358b8a378f3a6dca823ce
Gingertonic <bethmschofield@gmail.com>
2019-12-03 22:34:26 +0000

    init commit

---
+++ lib/chart/to/json.rb
@@ -0,0 +1,10 @@
+require "chart/to/json/version"
+
+module Chart
+  module To
+    module Json
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chart/to/json/version.rb
@@ -0,0 +1,7 @@
+module Chart
+  module To
+    module Json
+      VERSION = "0.1.0"
+    end
+  end
+end
