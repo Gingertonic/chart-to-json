0fe0d8bee96850ad8b8a1cea9384fe304dd0ca4e
Gingertonic <bethmschofield@gmail.com>
2019-12-04 00:19:56 +0000

    refined output

--- lib/chart_to_json/chart-convertor.rb
+++ lib/chart_to_json/chart-convertor.rb
@@ -13,7 +13,7 @@ class ChartToJson::ChartConvertor
 
     def convert 
         @original.each do |line|
-            if line.include?("verse") || line.include?("chorus")
+            if line.include?("verse") || line.include?("chorus") || line.include?("intro")
                 @current_section = line.chomp
                 @output[:structure] << @current_section
                 @output[:sections][@current_section.to_sym] = []
@@ -27,13 +27,14 @@ class ChartToJson::ChartConvertor
     def parseLine(line)
         changes = line.split("(")
         obj_line = []
-        changes.each do |change|
-            p change
+        changes.each_with_index do |change, i|
             if change.split(")").length >= 2
                 parts = change.split(")")
                 obj_line << {type: "chord", body: parts[0].chomp}
+                parts[1]+="-" if parts[1][-1] != " " && i != changes.length - 1
                 obj_line << {type: "lyric", body: parts[1].chomp}
             elsif !change.empty?
+                change+="-" if change[-1] != " " && i != changes.length - 1
                 obj_line << {type: "lyric", body: change.chomp}
             end 
         end
